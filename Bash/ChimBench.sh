48499c72ef4c27550ceb3e867df85051c21064d7
Sarah Djebali <sdjebali@users.noreply.github.com>
2016-05-10 14:08:16 +0200

    Update ChimBench.sh

--- Bash/ChimBench.sh
+++ Bash/ChimBench.sh
@@ -86,6 +86,10 @@ then
     echo "  in one and not in the other and a sensitivity and a precision measure (although for positive sets this precision is an underestimate of the true one" >&2
     echo "  since we do not know whether there are other chimeras to be found in this set)" >&2
     echo "- a 1 column file called common.txt with the coordinates of the common chimeric junctions" >&2
+    echo "- an 8 column tsv file called ref_junc_belonging_to_common_gnpairs_vs_pred_same.tsv with the predicted junctions (same as 1st input file) but" >&2 
+    echo "  with information about all the reference junctions sharing the same chr and strand for the two parts of the junction, their donor distance" >&2  
+    echo "  to the predicted junction donor, their acceptor distance to the predicted junction acceptor, the sum of those and the subset of sums that" >&2  
+    echo "  are minimum together with their associated reference junctions"
     echo "" >&2
     exit 1
 else
